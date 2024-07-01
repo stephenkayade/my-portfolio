4391aad643cdfa5d5d8c5967cc2f792de643a2c1
Oluwatobi Immanuel <stephenkayade@gmail.com>
2024-07-01 07:11:33 +0100

    adjusted spacing in the home page

--- src/components/partials/GitchText.tsx
+++ src/components/partials/GitchText.tsx
@@ -4,7 +4,7 @@ import { IGlitchText } from '../../utils/types'
 const GlitchText: FC<IGlitchText> = ({ text }) => {
     return (
         <>
-            <p className="text-white text-4xl glitch pb-24 2xl:pb-0 xl:pb-0 md:pb-0 lg:pb-8 pb-8">
+            <p className="text-white text-4xl glitch pb-20 2xl:pb-0 xl:pb-0 md:pb-0 lg:pb-8">
                 <span aria-hidden='true'>{text}</span>
                 <span aria-hidden='true'>{text}</span>
                 <span aria-hidden='true'>{text}</span>
