07835365d279dd4600090de4315e25e94bf44c8c
Stephen Adediran <oadediran@terraswitching.com>
2025-06-07 19:32:41 +0100

    updated app

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,8 +2,9 @@ import PageWrapper from '../../components/layouts/PageWrapper'
 import NextScreenButton from '../../components/partials/NextScreen'
 import TitleText from '../../components/partials/TitleText'
 import FixedText from '../../components/partials/FixedText'
-import {contactList } from '../../components/mocks'
+import { contactList } from '../../components/mocks'
 import ContactList from './ContactList'
+import { Link } from 'react-router-dom'
 
 const Contact = () => {
     return (
@@ -22,11 +23,32 @@ const Contact = () => {
 
                         <ContactList data={contactList} />
 
-                        <div className="mt-12">
+                    </div>
 
-                            <NextScreenButton text='Click to go home' link='' />
+                </div>
 
-                        </div>
+
+                <div className='xl:max-w-screen-md 2xl:max-w-screen-md md:max- max-w-screen-md mx-auto mb-12 pt-10'>
+
+                    <TitleText
+                        fontWeight='extrabold'
+                        text='Resume.'
+                        size='5xl'
+                    />
+
+                    <p className='pt-7'>
+                        <span>
+                            <a href="https://drive.google.com/drive/folders/1bJIKwlk_5s_0MppXWMd7WQBhfvxpsUKc" target='_blank' rel="noreferrer" className='text-md text-white font-medium'> view,</a>
+                        </span>
+                        <span className='text-slate-400 font-light'> or </span>
+                        <a href='../../../assets/files/Oluwakayode Adediran.pdf' download='Adediran Oluwakayode CV.pdf' className=' text-white font-medium'> download </a>
+                        <span className='text-slate-400 font-light'> my resume </span>
+                    </p>
+
+
+                    <div className="mt-12">
+
+                        <NextScreenButton text="Are you convinced to contact me now?" link='contact' />
 
                     </div>
 
